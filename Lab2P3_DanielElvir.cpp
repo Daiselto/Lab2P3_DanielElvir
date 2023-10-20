8126a52c81da4e07f9e39a6ff1f54f33a64d5225
Daniel Elvir <danielisrael.elvir@gmail.com>
2023-10-20 15:01:55 -0600

    Segundos avances de las 3 de la tarde

--- Lab2P3_DanielElvir.cpp
+++ Lab2P3_DanielElvir.cpp
@@ -6,6 +6,8 @@ int main() {
     int long1;
     int long2;
     int long3;
+    int opTemp;
+    float tempIni;
     cout << "Bienvenido a mi primer lab" << endl;
     cout << "1. Identificador de Triangulos" << endl;
     cout << "2. Convertidor de Temperaturas" << endl;
@@ -25,13 +27,32 @@ int main() {
             cin >> long3;
             if (long1 == long2 && long1 == long3 && long2 == long3) {
                 cout << "El triangulo es equilatero" << endl;
-            }else if (long1 == long2 && long2 == long3 && long1 != long3 || long2 == long3 && long1 == long3 && long2 != long3) {
+            }else if (long1 == long2 && long1 != long3 || long2 == long3 && long1 != long3 || long3 == long1 && long2 != long3) {
                 cout << "El triangulo es isoceles" << endl;
             }else if (long1 != long2 && long1 != long3 && long2 != long3) {
                 cout << "El triangulo es escaleno" << endl;
             }
             break;
         case 2:
+            cout << "1. Celsius" << endl;
+            cout << "2. Fahrenheit" << endl;
+            cout << "3. Kelvin" << endl;
+            cout << "Selecciona la unidad de medidad de la temperatura a ingresar" << endl;
+            cin >> opTemp;
+            if (opTemp == 1) {
+                cout << "Ingrese su temperatura en Celsius" << endl;
+                cin >> tempIni;
+                cout << "La temperatura en Farenheit " << tempIni * 1.8 + 32;
+                cout << "La temperatura en Kelvin " << tempIni + 273.15;
+            }else if (opTemp == 2) {
+                cout << "Ingrese su temperatura en Fahrenheit" << endl;
+                cin >> tempIni;
+            }else if (opTemp == 3) {
+                cout << "Ingrese su temperatura en Kelvin" << endl;
+                cin >> tempIni;
+            }else {
+                cout << "NO INGRESÓ UNA MEDIDA ADECUADA" << endl;
+            }
             break;
         case 3:
             break;
