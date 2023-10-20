c341d7cebee842af108c36fb22905f0f50f7fa3e
Daniel Elvir <danielisrael.elvir@gmail.com>
2023-10-20 14:29:52 -0600

    Avances pequeños

--- Lab2P3_DanielElvir.cpp
+++ Lab2P3_DanielElvir.cpp
@@ -1,19 +1,46 @@
-// Lab2P3_DanielElvir.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
-//
 
 #include <iostream>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+int main() {
+    int long1;
+    int long2;
+    int long3;
+    cout << "Bienvenido a mi primer lab" << endl;
+    cout << "1. Identificador de Triangulos" << endl;
+    cout << "2. Convertidor de Temperaturas" << endl;
+    cout << "3. Paridad del digito de las centenas" << endl;
+    cout << "4. Salir del Programa" << endl;
+    cout << "Ingrese una opcion valida" << endl;
+    int opcion;
+    cin >> opcion;
+
+    switch (opcion) {
+        case 1: 
+            cout << "Ingrese la primera longitud: ";
+            cin >> long1;
+            cout << "Ingrese la segunda longitud: ";
+            cin >> long2;
+            cout << "Ingrese la tercera longitud: ";
+            cin >> long3;
+            if (long1 == long2 && long1 == long3 && long2 == long3) {
+                cout << "El triangulo es equilatero" << endl;
+            }else if (long1 == long2 && long2 == long3 && long1 != long3 || long2 == long3 && long1 == long3 && long2 != long3) {
+                cout << "El triangulo es isoceles" << endl;
+            }else if (long1 != long2 && long1 != long3 && long2 != long3) {
+                cout << "El triangulo es escaleno" << endl;
+            }
+            break;
+        case 2:
+            break;
+        case 3:
+            break;
+        case 4:
+            break;
+
+        default: cout << "NO INGRESÓ UNA OPCION VALIDA!!!" << endl;;
+            break;
+    }
+
 }
 
-// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
-// Depurar programa: F5 o menú Depurar > Iniciar depuración
-
-// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
-//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
-//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
-//   4. Use la ventana Lista de errores para ver los errores
-//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
-//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
